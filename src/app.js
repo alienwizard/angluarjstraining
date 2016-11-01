4acf9b7e1960843afb17a3c25d8e4a19b5ee0982
alienwizard <johanwendin@gmail.com>
2016-11-01 17:03:45 +0100

    bootstraping

---
+++ src/app.js
@@ -0,0 +1,5 @@
+'use strict'
+
+import angular from 'angular';
+
+import './scss/style.scss'
---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+var webpack = require('webpack');
+var node_dir = __dirname + '/node_modules';
+
+module.exports = {
+	entry: ['bootstrap-loader','./src/app.js'],
+
+	output: {
+		path: './dist/',
+		filename: 'bundle.js'
+	},
+
+	module: {
+		loaders: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loaders: ['babel-loader']
+			},
+
+			{
+				test: /\.json$/,
+				exclude: /node_modules/,
+				loaders: ['json']
+
+			},
+
+			{
+				test: /\.scss$/,
+				loaders:['style-loader', 'css-loader', 'sass-loader']
+			},
+      {
+				test: /\.css$/,
+				loaders:['style-loader', 'css-loader', 'sass-loader']
+			},
+
+			{
+				test: /\.(jpg|png|woff|woff2|eot|ttf|svg|otf)$/,
+				loader: 'url-loader?limit=100000'
+			},
+
+		]
+
+	},
+
+	 plugins: [
+        new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jQuery"
+        })
+    ]
+
+}
+
